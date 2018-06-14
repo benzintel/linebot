7e4b039e4f1d471d4f5eebd8da25f6bc9ecca27a
sarut yentakham <sarut.y@bitkub.com>
2018-06-14 23:56:02 +0700

    Token

--- bot.php
+++ bot.php
@@ -5,7 +5,7 @@
 include ('line-bot.php');
 
 $channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
-$access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
+$access_token  = 'yZ9BNMGXsjRnV7rLCMXGBPkHczkLYQogduWkeD5oscd384zV057P5uPjgT4/EF6CaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c2fxYyXxoCyer2KL1uGUWQCeqkNnJ1K3Ql9ntJZNr61NAdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
