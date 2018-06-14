4336a62d9ac96eee04acc765db82da62f629a1da
sarut yentakham <sarut.y@bitkub.com>
2018-06-15 00:06:13 +0700

    update

--- bot.php
+++ bot.php
@@ -6,12 +6,17 @@ include ('line-bot.php');
 
 $channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
 $access_token  = 'yZ9BNMGXsjRnV7rLCMXGBPkHczkLYQogduWkeD5oscd384zV057P5uPjgT4/EF6CaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c2fxYyXxoCyer2KL1uGUWQCeqkNnJ1K3Ql9ntJZNr61NAdB04t89/1O/w1cDnyilFU=';
+$idPush = '8115269325570';
 
 $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
-		
-	$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+	
+	if($bot->message == 'สวัสดี'){
+		$bot->replyMessageNew($bot->replyToken, json_encode('ไงจ๊ะน้องหนู'));
+	}else{
+		$bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+	}
 
 	if ($bot->isSuccess()) {
 		echo 'Succeeded!';
