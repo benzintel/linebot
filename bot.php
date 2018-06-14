8f552e8b00a77d1bf5e5884cad981b90cbcab3e4
sarut yentakham <sarut.y@bitkub.com>
2018-06-14 23:45:14 +0700

    Update

--- bot.php
+++ bot.php
@@ -2,7 +2,7 @@
 
 // example: https://github.com/onlinetuts/line-bot-api/blob/master/php/example/chapter-01.php
 
-include ('core/line-bot.php');
+include ('line-bot.php');
 
 $channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
 $access_token  = '2og9ogezC8k.......5ZUEQQdB04t89/1O/w1cDnyilFU=';
