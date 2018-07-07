0ffc6757ca6bd7b520d1a096c20a78095cee29f7
sarut yentakham <sarut.y@bitkub.com>
2018-07-07 13:07:16 +0700

    Update Syntax

--- bot.php
+++ bot.php
@@ -10,94 +10,60 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-// if ( sizeof($request_array['events']) > 0 )
-// {
 
-// 	foreach ($request_array['events'] as $event){
-// 		$reply_message = '';
-// 		$reply_token = $event['replyToken'];
 
-// 		if ( $event['type'] == 'message' ) {
-// 			if( $event['message']['type'] == 'text' ) {
-// 			    $text = $event['message']['text'];
-// 			    $reply_message = 'ระบบได้รับข้อความ ('.$text.') ของคุณแล้ว 01';
-// 			}else{
-// 				$reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว 02';	  
-// 			}
-// 		}
-// 		else{
-// 			$reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว 03';
-			 
-// 			if( strlen($reply_message) > 0 ) {
-// 				//$reply_message = iconv("tis-620","utf-8",$reply_message);
-// 				$data = [
-// 					'replyToken' => $reply_token,
-// 					'messages' => [['type' => 'text', 'text' => $reply_message]]
-// 				];
-// 				$post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+if ( sizeof($request_array['events']) > 0 ) {
 
-// 				$send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-// 				echo "Result: ".$send_result."\r\n";
-// 			}
-// 		}
-// 	}
-// }
+    foreach ($request_array['events'] as $event) {
 
-if ( sizeof($request_array['events']) > 0 )
-{
+        $reply_message = '';
+        $reply_token = $event['replyToken'];
 
- foreach ($request_array['events'] as $event)
- {
-  $reply_message = '';
-  $reply_token = $event['replyToken'];
+        if ( $event['type'] == 'message' ) {
+            if( $event['message']['type'] == 'text' ) {
+                
+                $text = $event['message']['text'];
+                if( $text == 'สวัสดี') {
+                    $reply_message = 'สวัสดีนายท่าน ';
+                } else{
+                    $reply_message = 'สวัสดีนายท่าน '. $text;
+                }
+            } else { 
+                $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
+            }
+        } else {
+            $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
+        }
 
-  if ( $event['type'] == 'message' ) 
-  {
-   if( $event['message']['type'] == 'text' )
-   {
-    $text = $event['message']['text'];
-    if( $text == 'สวัสดี') {
-    	$reply_message = 'สวัสดีนายท่าน ';
-    }else{
-    	$reply_message = 'สวัสดีนายท่าน '. $text;
+        if( strlen($reply_message) > 0 )
+        {
+            //$reply_message = iconv("tis-620","utf-8",$reply_message);
+            $data = [
+            'replyToken' => $reply_token,
+            'messages' => [['type' => 'text', 'text' => $reply_message]]
+            ];
+            $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+
+            $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
+            echo "Result: ".$send_result."\r\n";
+        }
     }
-   }
-   else
-    $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
-  
-  }
-  else
-   $reply_message = 'ระบบได้รับ Event '.ucfirst($event['type']).' ของคุณแล้ว';
- 
-  if( strlen($reply_message) > 0 )
-  {
-   //$reply_message = iconv("tis-620","utf-8",$reply_message);
-   $data = [
-    'replyToken' => $reply_token,
-    'messages' => [['type' => 'text', 'text' => $reply_message]]
-   ];
-   $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
-
-   $send_result = send_reply_message($API_URL, $POST_HEADER, $post_body);
-   echo "Result: ".$send_result."\r\n";
-  }
- }
 }
 
 echo "OK";
 
 function send_reply_message($url, $post_header, $post_body)
 {
- $ch = curl_init($url);
- curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
- curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
- curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
- curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
- curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
- $result = curl_exec($ch);
- curl_close($ch);
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
 
- return $result;
+    return $result;
 }
 
 ?>
