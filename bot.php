c965c6316980ad1b8a2c11b13cbd429d8b31517e
sarut yentakham <sarut.y@bitkub.com>
2018-07-13 15:48:56 +0700

    Update

--- bot.php
+++ bot.php
@@ -43,8 +43,8 @@ if ( sizeof($request_array['events']) > 0 ) {
             //$reply_message = iconv("tis-620","utf-8",$reply_message);
             $data = [
             'replyToken' => $reply_token,
-            // 'messages' => [['type' => 'text', 'text' => $reply_message]]
-            'messages'  =>  '{"type":"bubble","header":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"header"}]},"hero":{"type":"image","url":"https://www.linefriends.com/img/img_sec.jpg","size":"full","aspectRatio":"2:1"},"body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"body"},{"type":"text","text":"body"}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"footer"}]}}'
+            'messages' => [['type' => 'text', 'text' => $reply_message]]
+            // 'messages'  =>  '{"type":"bubble","header":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"header"}]},"hero":{"type":"image","url":"https://www.linefriends.com/img/img_sec.jpg","size":"full","aspectRatio":"2:1"},"body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"body"},{"type":"text","text":"body"}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"footer"}]}}'
             ];
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
             $send_result = send_reply_message($API_FLEX_URL, $POST_HEADER, $post_body);
