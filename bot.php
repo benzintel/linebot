39e0258d0e83dc43fa3fe67fca3cffc46706f61e
sarut yentakham <sarut.y@bitkub.com>
2018-07-13 16:15:18 +0700

    Update

--- bot.php
+++ bot.php
@@ -51,12 +51,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
             $data = [
                 'to' => $event['source']['userId'],
-                'messages' => [
-                    [
-                        'type' => 'text', 
-                        'text' => 'testtttt'
-                    ]
-                ]
+                'messages' => json_decode('{"type":"bubble","header":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"header"}]},"hero":{"type":"image","url":"https://www.linefriends.com/img/img_sec.jpg","size":"full","aspectRatio":"2:1"},"body":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"body"},{"type":"text","text":"body"}]},"footer":{"type":"box","layout":"vertical","contents":[{"type":"text","text":"footer"}]}}', TRUE)
             ];
 
             $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
