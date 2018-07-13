3e119a26d40b7941ee730a624e56726d8b0c7ba0
sarut yentakham <sarut.y@bitkub.com>
2018-07-13 16:20:12 +0700

    Update

--- bot.php
+++ bot.php
@@ -59,7 +59,7 @@ if ( sizeof($request_array['events']) > 0 ) {
                 ]
             ];
 
-            $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
+            $post_body = json_encode($data);
 
             $send_result = send_reply_message($API_URL.'/push', $POST_HEADER, $post_body);
 
