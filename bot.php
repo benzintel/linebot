85ea36ecde62cefca61c90e0a6e0707f26ca0969
sarut yentakham <sarut.yen@thanachart.co.th>
2019-02-15 10:13:22 +0700

    same text response

--- bot.php
+++ bot.php
@@ -20,9 +20,11 @@ if ( sizeof($request_array['events']) > 0 ) {
         $reply_message = '';
         $reply_token = $event['replyToken'];
 
+        $text = $event['message']['text'];
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($request_array)]]
+            // 'messages' => [['type' => 'text', 'text' => json_encode($request_array) ]]  Debug Detail message
+            'messages' => [['type' => 'text', 'text' => $text ]]
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
