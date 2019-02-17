cadc8fe383d45d4a69a967bfb2c2cf7a9cad5f4c
sarut yentakham <>
2019-02-17 13:47:14 +0700

    debug

--- bot-flex.php
+++ bot-flex.php
@@ -142,7 +142,7 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => [['type' => 'text', 'text' => json_encode($jsonFlex) ]]
+            'messages' => $jsonFlex
         ];
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
