fbf7df01cf984e87177385f90d170b9404e2afd1
sarut yentakham <>
2019-02-17 13:50:34 +0700

    debug

--- bot-flex.php
+++ bot-flex.php
@@ -142,8 +142,11 @@ if ( sizeof($request_array['events']) > 0 ) {
 
         $data = [
             'replyToken' => $reply_token,
-            'messages' => $jsonFlex
+            'messages' => [$jsonFlex]
         ];
+
+        print_r($data);
+
         $post_body = json_encode($data, JSON_UNESCAPED_UNICODE);
 
         $send_result = send_reply_message($API_URL.'/reply', $POST_HEADER, $post_body);
