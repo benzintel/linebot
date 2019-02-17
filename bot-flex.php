72e71f77a9a74545c4c66419d21c78ba97f984e7
sarut yentakham <>
2019-02-17 13:55:56 +0700

    debug

--- bot-flex.php
+++ bot-flex.php
@@ -11,124 +11,124 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
 
-$jsonFlex = '{
-    "type": "flex",
-    "altText": "Flex Message",
-    "contents": {
-      "type": "bubble",
-      "direction": "ltr",
-      "header": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "text",
-            "text": "Purchase",
-            "size": "lg",
-            "align": "start",
-            "weight": "bold",
-            "color": "#009813"
-          },
-          {
-            "type": "text",
-            "text": "฿ 100.00",
-            "size": "3xl",
-            "weight": "bold",
-            "color": "#000000"
-          },
-          {
-            "type": "text",
-            "text": "Rabbit Line Pay",
-            "size": "lg",
-            "weight": "bold",
-            "color": "#000000"
-          },
-          {
-            "type": "text",
-            "text": "2019.02.14 21:47 (GMT+0700)",
-            "size": "xs",
-            "color": "#B2B2B2"
-          },
-          {
-            "type": "text",
-            "text": "Payment complete.",
-            "margin": "lg",
-            "size": "lg",
-            "color": "#000000"
-          }
+$jsonFlex = [
+    "type" => "flex",
+    "altText" => "Flex Message",
+    "contents" => [
+      "type" => "bubble",
+      "direction" => "ltr",
+      "header" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "Purchase",
+            "size" => "lg",
+            "align" => "start",
+            "weight" => "bold",
+            "color" => "#009813"
+          ],
+          [
+            "type" => "text",
+            "text" => "฿ 100.00",
+            "size" => "3xl",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "Rabbit Line Pay",
+            "size" => "lg",
+            "weight" => "bold",
+            "color" => "#000000"
+          ],
+          [
+            "type" => "text",
+            "text" => "2019.02.14 21 =>47 (GMT+0700)",
+            "size" => "xs",
+            "color" => "#B2B2B2"
+          ],
+          [
+            "type" => "text",
+            "text" => "Payment complete.",
+            "margin" => "lg",
+            "size" => "lg",
+            "color" => "#000000"
+          ]
         ]
-      },
-      "body": {
-        "type": "box",
-        "layout": "vertical",
-        "contents": [
-          {
-            "type": "separator",
-            "color": "#C3C3C3"
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "margin": "lg",
-            "contents": [
-              {
-                "type": "text",
-                "text": "Merchant",
-                "align": "start",
-                "color": "#C3C3C3"
-              },
-              {
-                "type": "text",
-                "text": "BTS 01",
-                "align": "end",
-                "color": "#000000"
-              }
+      ],
+      "body" => [
+        "type" => "box",
+        "layout" => "vertical",
+        "contents" => [
+          [
+            "type" => "separator",
+            "color" => "#C3C3C3"
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "Merchant",
+                "align" => "start",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "BTS 01",
+                "align" => "end",
+                "color" => "#000000"
+              ]
             ]
-          },
-          {
-            "type": "box",
-            "layout": "baseline",
-            "margin": "lg",
-            "contents": [
-              {
-                "type": "text",
-                "text": "New balance",
-                "color": "#C3C3C3"
-              },
-              {
-                "type": "text",
-                "text": "฿ 45.57",
-                "align": "end"
-              }
+          ],
+          [
+            "type" => "box",
+            "layout" => "baseline",
+            "margin" => "lg",
+            "contents" => [
+              [
+                "type" => "text",
+                "text" => "New balance",
+                "color" => "#C3C3C3"
+              ],
+              [
+                "type" => "text",
+                "text" => "฿ 45.57",
+                "align" => "end"
+              ]
             ]
-          },
-          {
-            "type": "separator",
-            "margin": "lg",
-            "color": "#C3C3C3"
-          }
+          ],
+          [
+            "type" => "separator",
+            "margin" => "lg",
+            "color" => "#C3C3C3"
+          ]
         ]
-      },
-      "footer": {
-        "type": "box",
-        "layout": "horizontal",
-        "contents": [
-          {
-            "type": "text",
-            "text": "View Details",
-            "size": "lg",
-            "align": "start",
-            "color": "#0084B6",
-            "action": {
-              "type": "uri",
-              "label": "View Details",
-              "uri": "https://google.co.th/"
-            }
-          }
+      ],
+      "footer" => [
+        "type" => "box",
+        "layout" => "horizontal",
+        "contents" => [
+          [
+            "type" => "text",
+            "text" => "View Details",
+            "size" => "lg",
+            "align" => "start",
+            "color" => "#0084B6",
+            "action" => [
+              "type" => "uri",
+              "label" => "View Details",
+              "uri" => "https://google.co.th/"
+            ]
+          ]
         ]
-      }
-    }
-  }';
+      ]
+    ]
+  ];
 
 
 
